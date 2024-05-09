a24b48d7b271dad861444b946919e980b6494841
Giacomo Vassallo <51168109+GiecV@users.noreply.github.com>
2024-05-09 12:26:20 +0200

    Update LiDAR.py

--- LiDAR.py
+++ LiDAR.py
@@ -2,9 +2,10 @@ import math
 import numpy as np
 
 class LiDAR:
-    def __init__(self, angle_interval=[0, 2*math.pi], position = (0,0), reach=10):
+    def __init__(self, angle_interval=[0, 2*math.pi], obstacles=[], n=10, reach=10):
         self.angle_interval = angle_interval
-        self.position = position
+        self.obstacles = obstacles
+        self.n = n
         self.reach = reach
 
     def line(p1, p2): #function for finding line coefficients given two points that define the line
@@ -24,15 +25,15 @@ class LiDAR:
         else:
             return False
 
-    def measure_at_angle(self, angle, obstacles): #function for measuring the distance between the robot position in a certain direction
+    def measure_at_angle(self, angle, position): #function for measuring the distance between the robot position in a certain direction
 
-        max_reach_point = (self.position[0] + (self.reach*np.cos(angle)), self.position[1] + (self.reach*np.sin(angle))) #finding the maximum point for computing intersections
-        ray = ((self.position[0], self.position[1]),(max_reach_point[0], max_reach_point[1])) #generating the ray
+        max_reach_point = (position[0] + (self.reach*np.cos(angle)), position[1] + (self.reach*np.sin(angle))) #finding the maximum point for computing intersections
+        ray = ((position[0], position[1]),(max_reach_point[0], max_reach_point[1])) #generating the ray
 
         measured_distance = 2*self.reach #dummy value for beginning the procedure
         intersection_point = None
 
-        for obstacle in obstacles: #repeat for each obstacle
+        for obstacle in self.obstacles: #repeat for each obstacle
         
           L1 = self.line(ray[0], ray[1]) #find coefficients of the ray
           L2 = self.line(obstacle[0], obstacle[1]) #find coefficients of the obstacle
@@ -41,9 +42,9 @@ class LiDAR:
           if not R: #if there is no intersection, the "out of range" value is sensed
             print("No intersection found with ", obstacle, ", getting maximum range")
           else: #there is an intersection
-            distance = math.dist(self.position, R) #computing the distance between the actual position and the intersection
+            distance = math.dist(position, R) #computing the distance between the actual position and the intersection
             if distance < measured_distance and distance <= self.reach: #if it is the current minimum distance and it is in reach
-              if ((self.position[0]<=R[0]<=max_reach_point[0]) or (max_reach_point[0]<=R[0]<=self.position[0])) and ((self.position[1]<=R[1]<=max_reach_point[1]) or (max_reach_point[1]<=R[1]<=self.position[1])):
+              if ((position[0]<=R[0]<=max_reach_point[0]) or (max_reach_point[0]<=R[0]<=position[0])) and ((position[1]<=R[1]<=max_reach_point[1]) or (max_reach_point[1]<=R[1]<=position[1])):
               #if it is in the direction of the ray
                 if ((obstacle[0][0]<=R[0]<=obstacle[1][0]) or (obstacle[1][0]<=R[0]<=obstacle[0][0])) and ((obstacle[0][1]<=R[1]<=obstacle[1][1]) or (obstacle[1][1]<=R[1]<=obstacle[0][1])):
                 #if the intersection is actually in the obstacle (not just in the direction of the obstacle)
@@ -56,15 +57,15 @@ class LiDAR:
         else: #else return the measured distance and the intersection point
           return measured_distance, intersection_point
         
-    def measure(self, n, obstacles):
-        if n<=0:
+    def measure(self, position):
+        if self.n<=0:
             raise ValueError('The number of rays must be positive')
         
-        angles = np.linspace(self.angle_interval[0], self.angle_interval[1], n+1)
+        angles = np.linspace(self.angle_interval[0], self.angle_interval[1], self.n +1)
         measurements = {}
 
         for angle in angles:
-          measurement, intersection_point = self.measure_at_angle(angle, obstacles)
+          measurement, intersection_point = self.measure_at_angle(angle, position)
           measurements[angle] = (measurement, intersection_point)
 
         return measurements
